9967a7519607016b8aefe198d5b382336312919e
Muhammed <mohamedabdomm2@outlook.sa>
2026-05-05 16:42:47 +0300

    edit target name

--- helper_functions.py
+++ helper_functions.py
@@ -24,6 +24,6 @@ def make_predictions(data_filepath, model_filepath):
         
     # Generate predictions
     y_test_pred = model.predict(X_test)
-    # Put predictions into Series with name "bankrupt", and same index as X_test
-    y_test_pred = pd.Series(y_test_pred, index=X_test.index, name="bankrupt")
+    # Put predictions into Series with name "Churn", and same index as X_test
+    y_test_pred = pd.Series(y_test_pred, index=X_test.index, name="Churn")
     return y_test_pred
